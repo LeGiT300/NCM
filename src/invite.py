30d832d4e39667be1e846cd05e7773ccb8aca9c2
Leku Genuine Tita <titaleku003@gmail.com>
2025-03-19 04:57:02 +0100

    Update .gitignore to exclude .txt files and modify invite.py to send a new message with error handling

--- src/invite.py
+++ src/invite.py
@@ -1,15 +1,16 @@
 import pywhatkit as kit
 import time
 
-# Define recipient numbers
 numbers = ["+237678931432"]
 
-# Define the message
-message = "Hello! This is an automated message from Python."
+message = "https://docs.expo.dev/router/advanced/stack/"
 
-# Send messages one by one
-for number in numbers:
-    kit.sendwhatmsg_instantly(number, message, wait_time=15)
-    time.sleep(10)  # Pause to avoid spam detection
+try:
+    for number in numbers:
+        kit.sendwhatmsg_instantly(number, message, wait_time=15)
+        time.sleep(10)
 
-print("Messages sent successfully!")
+    print("Messages sent successfully!")
+
+except Exception as e:
+    print('Error{}'.format(e))
